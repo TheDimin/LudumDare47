a830606ddf6487ee377635d7830c140deeaa1cca
Jesper Enschede <jesperensched@gmail.com>
2020-10-03 21:03:55 +0200

    Update gitignore
    Auto stash before merge of "main" and "origin/main"
    Removed file

---
+++ Assets/LudumDare47/Scripts/Minigame/MinigameController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+namespace LD47
+{
+    namespace Minigame
+    {
+        public class MinigameController : MonoBehaviour
+        {
+            private PCUiController pcUI;
+
+            [SerializeField] private List<Module> modules = new List<Module>();
+
+            private void Start() {
+                pcUI = GameObject.FindObjectOfType<PCUiController>();
+                
+                for (int i = 0; i < 9; i++) {
+                    Module module = new Module();
+                    module.type = (ModuleType)Random.Range(0, Enum.GetNames(typeof(ModuleType)).Length);
+                    
+                    modules.Add(module);
+                }
+                
+                for (int i = 0; i < modules.Count; i++) {
+                    
+                    // calculate west neightbour
+                    if ((i - 1) >= 0) {
+                        modules[i].west = i - 1;
+                    }
+                    else {
+                        modules[i].west = -1;
+                    }
+                    
+                    // calculate east neightbour
+                    if ((i + 1) <= modules.Count - 1) {
+                        modules[i].east = i + 1;              
+                    }
+                    else {
+                        modules[i].east = -1;
+                    }
+                    
+                    // calculate north  neightbour
+                    if ((i - 3) >= 0) {
+                        modules[i].north = i - 3;             
+                    }
+                    else {
+                        modules[i].north = -1;                    
+                    }
+                    
+                    // calculate south neightbour
+                    if ((i + 3) <= modules.Count) {
+                        modules[i].south = i + 3;               
+                    }
+                    else {
+                        modules[i].south = -1;                  
+                    }
+                }
+
+                pcUI.create_module_spot_button_from_list(modules.ToArray());
+            }
+            
+            private bool solve_puzzel() {
+                return false;
+            }
+            
+            public void set_module(int index, ModuleType type) {
+                modules[index].type = type;
+            }
+
+            public Module get_module(int index) {
+                return modules[index];
+            }
+        }
+    }
+}
+
---
+++ Assets/LudumDare47/Scripts/Minigame/PCUiController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace LD47
+{
+    namespace Minigame
+    {
+        public class PCUiController : MonoBehaviour
+        {
+            public List<Button> moduleSpotButtons;
+            public Button[] moduleButtons;
+
+            private Button selectedModuleButton;
+
+            private Button runRobotButton;
+            private Button discardRobotButton;
+
+            private Button ludumDareButton;
+            private TMPro.TMP_Text timeText;
+
+            [SerializeField] private GameObject moduleSlotPrefab;
+            // temp
+            [SerializeField]private Sprite[] sprites;
+            
+            private void Awake() {
+                moduleButtons = GameObject.Find("PCBContent").GetComponentsInChildren<Button>();
+                
+                runRobotButton = GameObject.Find("RunRobotButton").GetComponent<Button>();
+                runRobotButton.onClick.AddListener(()=>{ /* signal mini game manager that the pcb plate needs to be checked */ });
+                
+                discardRobotButton = GameObject.Find("DiscardRobotButton").GetComponent<Button>();
+                discardRobotButton.onClick.AddListener(()=>{ /* signal mini game manager that the robot is broken and needs to be discarded*/ });
+
+                ludumDareButton = GameObject.Find("LudumDareButton").GetComponent<Button>();
+                ludumDareButton.onClick.AddListener(()=>{ Application.OpenURL("https://ldjam.com/"); /* little easter egg :) */ });
+
+                timeText = GameObject.Find("OSTimeText").GetComponent<TMPro.TMP_Text>();
+                
+                for (int i = 0; i < moduleButtons.Length; i++) {
+                    int x = i;
+                    moduleButtons[i].onClick.AddListener(() =>
+                    {
+                        selectedModuleButton = moduleButtons[x];
+                    });
+                }
+            }
+
+            public void create_module_spot_button_from_list(Module[] modules) {
+                for (int i = 0; i < modules.Length; i++) {
+                    var a = Instantiate(moduleSlotPrefab, GameObject.Find("_SLOTGRID").transform);
+                    moduleSpotButtons.Add(a.GetComponent<Button>());
+                    moduleSpotButtons[i].GetComponentInChildren<Button>().image.sprite = sprites[(int)modules[i].type];
+                    
+                    int x = i;
+                    moduleSpotButtons[i].onClick.AddListener(() =>
+                    {
+                        if (selectedModuleButton != null) {
+                            selectedModuleButton.transform.position = moduleSpotButtons[x].transform.position;
+                            selectedModuleButton.transform.SetParent(moduleSpotButtons[x].transform);
+                            selectedModuleButton = null;
+                        }
+                    });
+                }
+            }
+
+            private void LateUpdate() {
+                timeText.text = DateTime.Now.ToString("h:mm:ss tt");
+            }
+        }
+    }
+}
+
