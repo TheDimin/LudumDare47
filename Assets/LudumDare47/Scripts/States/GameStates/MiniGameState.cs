5a541d616e3fce7e1b26602c947e1b31b2667211
Damian <d.p.hoorn@outlook.com>
2020-10-04 21:56:44 +0200

    Magic scene stuff

--- Assets/LudumDare47/Scripts/States/GameStates/MiniGameState.cs
+++ Assets/LudumDare47/Scripts/States/GameStates/MiniGameState.cs
@@ -1,29 +1,39 @@
-﻿using Tools.StateManager;
+﻿using Cinemachine;
+using LD47.Minigame;
+using Tools.StateManager;
 using UnityEngine;
 
 namespace LD47.GameStates
 {
     public class MiniGameState : GameStateBase
     {
+        private StateManagerBase<PCState> pcState;
+
         public override void OnEnterState()
         {
+            if (pcState == null)
+                pcState = PCController.Instance.pcStateManager;
+            
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
+
+            gameManager.Player.GetComponentInChildren<CinemachineVirtualCamera>().gameObject.SetActive(false);
+
         }
 
         public override void OnExitState()
         {
-            throw new System.NotImplementedException();
+            gameManager.Player.GetComponentInChildren<CinemachineVirtualCamera>().gameObject.SetActive(true);
         }
 
         public override bool CanEnter(StateBase currentStateBase)
         {
-            throw new System.NotImplementedException();
+            return pcState.GetState().GetType() == typeof(PCPlayState);
         }
 
         public override bool CanExit()
         {
-            throw new System.NotImplementedException();
+            return pcState.GetState().GetType() != typeof(PCPlayState);
         }
     }
 }
