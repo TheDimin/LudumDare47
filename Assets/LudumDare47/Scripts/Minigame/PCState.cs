da0665c378a8ec545222ae17025a97ff037aaee3
Jesper Enschede <jesperensched@gmail.com>
2020-10-04 15:56:22 +0200

    Fixed the PC ui

---
+++ Assets/LudumDare47/Scripts/Minigame/PCState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tools.StateManager;
+using UnityEngine;
+
+namespace LD47
+{
+    namespace Minigame
+    {
+        public abstract class PCState : StateBase
+        {
+            
+        }
+    }
+}
--- Assets/LudumDare47/Scripts/Minigame/PCUiController.cs
+++ Assets/LudumDare47/Scripts/Minigame/PCUiController.cs
@@ -11,37 +11,14 @@ namespace LD47
     {
         public class PCUiController : MonoBehaviour
         {
-            public List<Button> moduleSpotButtons;
-            public Button[] moduleButtons;
-
-            private Button selectedModuleButton;
-
-            private Button runRobotButton;
-            private Button discardRobotButton;
-
             private Button ludumDareButton;
             private TMPro.TMP_Text timeText;
 
-            [SerializeField] private GameObject moduleSlotPrefab;
-            // temp
-            [SerializeField]private Sprite[] sprites;
-            
             private void Awake() {
-                runRobotButton = GameObject.Find("RunRobotButton").GetComponent<Button>();
-                runRobotButton.onClick.AddListener(()=>{ });
-                
-                discardRobotButton = GameObject.Find("DiscardRobotButton").GetComponent<Button>();
-                discardRobotButton.onClick.AddListener(()=>{ /* signal mini game manager that the robot is broken and needs to be discarded*/ });
-
                 ludumDareButton = GameObject.Find("LudumDareButton").GetComponent<Button>();
                 ludumDareButton.onClick.AddListener(()=>{ Application.OpenURL("https://ldjam.com/"); /* little easter egg :) */ });
 
                 timeText = GameObject.Find("OSTimeText").GetComponent<TMPro.TMP_Text>();
-                
-            }
-
-            public void create_module_spot_button_from_list(Module[] modules) {
-
             }
 
             private void LateUpdate() {
