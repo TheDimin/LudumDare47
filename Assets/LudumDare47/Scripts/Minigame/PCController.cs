5fe431a29192a3611662d9158e1031ea04175149
Jesper Enschede <jesperensched@gmail.com>
2020-10-04 15:59:27 +0200

    Added pc states

---
+++ Assets/LudumDare47/Scripts/Minigame/PCController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Tools.StateManager;
+using UnityEngine;
+
+namespace LD47
+{
+    namespace Minigame
+    {
+        public class PCController : MonoBehaviour
+        {
+            private StateManagerBase<PCState> pcStateManager;
+            
+            private void Start() {
+                pcStateManager = new StateManagerBase<PCState>();
+            }
+
+            private void Update() {
+                pcStateManager.Update();
+            }
+        }
+    }
+}
+
